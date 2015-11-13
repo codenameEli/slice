d6bef8b808f0cb48ce53b2ae257acdfeade458dd
Eli Dalbey <eli@launchdm.com>
2015-11-13 12:20:01 -0500

    Update index

--- index.php
+++ index.php
@@ -1,16 +1,29 @@
 <?php
-	$slices = json_encode( scandir('./slices') );
+	// use Slice as Slice;
+	// require 'vendor/autoload.php';
+
+	require __DIR__ . '/vendor/autoload.php';
+
+	use Slice\SliceUtils as SliceUtils;
+	use Slice\Slices as Slices;
+
+	$Slices = new Slices();
+	$slices = $Slices->get_slices();
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Slice testing</title>
+	<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+	<script type="text/javascript" src="bower_components/jquery-ui/jquery-ui.min.js"></script>
 	<script type="text/javascript" src="bower_components/angular/angular.min.js"></script>
 	<script type="text/javascript" src="bower_components/ng-module/ng-module.js"></script>
 	<script type="text/javascript" src="src/slice.min.js"></script>
 
+	<link rel="stylesheet" type="text/css" href="src/css/slice.min.css">
+
 	<script type="text/javascript">
-		var slices = <?php echo $slices; ?>;
+		var slicesJSON = <?php echo $slices; ?>;
 	</script>
 </head>
 <body>
@@ -18,12 +31,41 @@
 
 	<div ng-module="Slice">
 		<div ng-controller="SliceCtrl">
-			<code>{{ slice }}</code>
-			<img ng-repeat="src in slices" src="{{ src }}" />
-			<input type="file" class="drag-drop" drag-drop multiple></input>
+			<!-- <div ng-repeat="slice in slices">
+				<img ng-src="/slices/{{ slice }}" />
+			</div> -->
+
+			<div class="controls-container" ng-class="{ hidden: mainToolbarVisible }">
+				<div class="slice-main-toolbar">
+					SLICE
+					<div class="toggle-main-toolbar" ng-click="toggleMainToolbar()">
+						<span>
+							<i class="fa fa-chevron-up" ng-show="mainToolbarVisible"></i>
+						</span>
+
+						<span>
+							<i class="fa fa-chevron-down" ng-show="!mainToolbarVisible"></i>
+						</span>
+					</div>
+				</div>
+				<ce-controls
+				class="slice-controls-container"
+				ng-class="{ 'active': slice.visible }"
+				ng-repeat="slice in slices"></ce-controls>
+			</div>
+			<!-- <input type="file" class="drag-drop" drag-drop multiple></input> -->
+
+			<ce-slice
+				class="slice-container"
+				ng-repeat="slice in slices"
+				ng-class="{ 'active': slice.visible }"
+				ce-draggable
+				style="left: {{ slice.position.left }}px; top: {{ slice.position.top }}px; opacity: {{ slice.opacity }}"
+			></ce-slice>
 		</div>
 	</div>
 </body>
+
 
 <style type="text/css">
 	.drag-drop {
